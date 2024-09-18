2dcc87a688f38c0403d3e4ec2d68044da91a35aa
huu thiet <huuthietn01@gmail.com>
2024-09-18 08:38:38 +0700

    add: custom decorate IsDateString to tranfer parameter

---
+++ app/backend/src/exception/custom-decorate.ts
@@ -0,0 +1,20 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export function IsCustomDateString(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isCustomDateString',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: unknown) {
+          return typeof value === 'string' && !isNaN(Date.parse(value));
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be a valid ISO 8601 date string!`;
+        },
+      },
+    });
+  };
+}
