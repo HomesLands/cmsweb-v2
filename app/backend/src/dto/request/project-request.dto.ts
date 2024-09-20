5760e49fc170e8419ad903423b188745d2a7e71a
tuanhcmute <20110743@student.hcmute.edu.vn>
2024-09-20 14:48:49 +0700

    add quickstart for docs

--- app/backend/src/dto/request/project-request.dto.ts
+++ app/backend/src/dto/request/project-request.dto.ts
@@ -1,35 +1,35 @@
 import { AutoMap } from "@automapper/classes";
 import { Expose } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
-import { IsCustomDateString } from "@decorate";
+import { IsDateStringWithMessage } from "decorator";
 
 export class CreateProjectRequestDto {
-  @IsNotEmpty({message: "INVALID_PROJECT_NAME"})
+  @IsNotEmpty({ message: "INVALID_PROJECT_NAME" })
   @Expose()
   @AutoMap()
   name?: string;
 
-  @IsNotEmpty({message: "INVALID_PROJECT_START_DATE"})
-  @IsCustomDateString({message: "INVALID_DATE_FORMAT"})
+  @IsNotEmpty({ message: "INVALID_PROJECT_START_DATE" })
+  @IsDateStringWithMessage({ message: "INVALID_DATE_FORMAT" })
   @Expose()
   startDate?: string;
 
-  @IsNotEmpty({message: "INVALID_PROJECT_PROCESS"})
+  @IsNotEmpty({ message: "INVALID_PROJECT_PROCESS" })
   @Expose()
   @AutoMap()
   process?: number;
 
-  @IsNotEmpty({message: "INVALID_PROJECT_DESCRIPTION"})
+  @IsNotEmpty({ message: "INVALID_PROJECT_DESCRIPTION" })
   @Expose()
   @AutoMap()
   description?: string;
 
-  @IsNotEmpty({message: "INVALID_PROJECT_FILE_DESCRIPTION"})
+  @IsNotEmpty({ message: "INVALID_PROJECT_FILE_DESCRIPTION" })
   @Expose()
   @AutoMap()
   fileDescription?: string;
 
-  @IsNotEmpty({message: "INVALID_PROJECT_MANAGER"})
+  @IsNotEmpty({ message: "INVALID_PROJECT_MANAGER" })
   @Expose()
   manager?: string;
 }
